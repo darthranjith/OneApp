e975b06a1c6d300c933fbf13a82ce77c6e0f7bfe
darthranjith <srivaranjith@gmail.com>
2019-12-05 22:19:35 +0530

    Improvements SPICinemas Ticket booking * Scraps every 1 minute * Progress bar * Improved api calls

--- projects/spicinemas-lib/src/lib/components/spicinemas/spicinemas.component.ts
+++ projects/spicinemas-lib/src/lib/components/spicinemas/spicinemas.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {SpicinemasService} from '../../services/spicinemas.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {interval, Subscription} from 'rxjs';
-import {flatMap} from 'rxjs/operators';
+import {combineLatest, interval, merge, Observable, pipe, Subscription, timer} from 'rxjs';
+import {first, flatMap, map, switchMap, take, takeWhile, tap} from 'rxjs/operators';
 import {TerminalMovie} from '../../models/terminal_movie';
 
 @Component({
@@ -10,56 +10,71 @@ import {TerminalMovie} from '../../models/terminal_movie';
   templateUrl: './spicinemas.component.html',
   styleUrls: ['./spicinemas.component.scss']
 })
-export class SpicinemasComponent implements OnInit {
-
+export class SpicinemasComponent implements OnInit, OnDestroy {
+  timer = 10;
   movies: string[];
   movieStatus: TerminalMovie = null;
   movieForm = new FormGroup({
     movie: new FormControl('', Validators.required)
   });
-  apicall: Subscription;
   isActive: boolean;
+  @ViewChild('progress', {static: false}) progress: ElementRef<HTMLElement>;
+  source$ = timer(0, 60000);
+  source2$ = timer(0, 1000);
+  private movieSubscription$: Subscription;
+  private timerSubscription$: Subscription;
+  private apiSubscription$: Subscription;
 
   constructor(private service: SpicinemasService) {
     this.isActive = false;
   }
 
   ngOnInit() {
-    this.service.getAllComingSoonMovies().subscribe(res => {
+    this.movieSubscription$ = this.service.getAllComingSoonMovies().subscribe(res => {
       const response: any = res;
       this.movies = response.movies;
     });
+
   }
 
   getStatus() {
     this.isActive = true;
-    this.apicall = interval(10 * 60 * 1000)
-      .pipe(
-        flatMap(() => this.service.checkStatus(this.movieForm.value.movie))
-      )
-      .subscribe(res => {
-        const response: any = res;
-        if (!response.status) {
-          this.movieStatus = {
-            movie: this.movieForm.value.movie,
-            status: response.status,
-            message: 'Booking not yet opened!'
-          };
-        } else {
-          this.movieStatus = {
-            movie: this.movieForm.value.movie,
-            status: response.status,
-            message: 'Booking opened!'
-          };
-          this.playAudio();
-          this.apicall.unsubscribe();
-        }
-      });
+    this.apiSubscription$ = this.source$.pipe(
+      switchMap(() => {
+        const counter$ = this.source2$.pipe(
+          tap((timer: number) => {
+            this.timer = 60 - timer;
+            this.progress.nativeElement.style.width = (timer / 60) * 100 + '%';
+          })
+        );
+        const status$ = this.service.checkStatus(this.movieForm.value.movie).pipe(
+          first(),
+          tap((response: any) => {
+            if (!response.status) {
+              this.movieStatus = {
+                movie: this.movieForm.value.movie,
+                status: response.status,
+                message: 'Booking not yet opened!'
+              };
+            } else {
+              this.movieStatus = {
+                movie: this.movieForm.value.movie,
+                status: response.status,
+                message: 'Booking opened!'
+              };
+              this.playAudio();
+              this.apiSubscription$.unsubscribe();
+            }
+          })
+        );
+        return combineLatest(counter$, status$);
+      }),
+  ).subscribe();
   }
 
   stop() {
     this.isActive = false;
-    this.apicall.unsubscribe();
+    this.apiSubscription$.unsubscribe();
   }
 
   playAudio() {
@@ -67,5 +82,11 @@ export class SpicinemasComponent implements OnInit {
     audio.src = '../../../assets/audio/alert.mp3';
     audio.load();
     setInterval(() => audio.play(), 1000);
+  }
+
+  ngOnDestroy(): void {
+    this.movieSubscription$.unsubscribe();
+    this.timerSubscription$.unsubscribe();
+    this.apiSubscription$.unsubscribe();
   }
 }
--- projects/spicinemas-lib/src/lib/constants/url.ts
+++ projects/spicinemas-lib/src/lib/constants/url.ts
@@ -1,5 +1,5 @@
 export enum Url {
-  BASE_URL = 'http://api.varanjith.com/spicinemas/',
+  BASE_URL = 'https://api.varanjith.com/spicinemas/',
   GET_ALL_COMING_SOON_MOVIES = 'getcomingsoonlist',
   CHECK_STATUS = 'checkbookingstatus?movie={PARAM}'
 }
